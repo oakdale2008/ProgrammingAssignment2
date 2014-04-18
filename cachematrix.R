8ad361e5a61a1b630475f28c5eeb7415d1f2f98e
oakdale2008 <darren_gilligan@yahoo.co.uk>
2014-04-18 03:25:24 +0100

    adding comments to the r file

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions below which are used to create and modify an object
+## to store both a matrix and it's inverse together
 
-## Write a short comment describing this function
 
+## makeCacheMatrix caches accepts a matrix as a argument and
+## returns a list allowing user to set/get the matrix itself,
+## and set or get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {  
   
   invmatrix <- NULL
@@ -25,8 +27,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve accepts the list created by makeCacheMatrix as an argument
+## and returns the inverse of matrix from either the cache or a calculated
+## inverse. It will populate the cache if empty
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getInv()
